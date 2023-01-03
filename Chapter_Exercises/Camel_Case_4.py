957b3d9c15b1505eb2d5e38456d1cdbc89839951
zhangxt2001 <116774353+zhangxt2001@users.noreply.github.com>
2023-01-02 20:15:22 -0500

    Update Camel_Case_4.py

--- Chapter_Exercises/Camel_Case_4.py
+++ Chapter_Exercises/Camel_Case_4.py
@@ -1,3 +1,34 @@
+'''
+##########Sample Input##########
+
+S;M;plasticCup()
+
+C;V;mobile phone
+
+C;C;coffee machine
+
+S;C;LargeSoftwareBook
+
+C;M;white sheet of paper
+
+S;V;pictureFrame
+
+#######Sample Output#################
+
+plastic cup
+
+mobilePhone
+
+CoffeeMachine
+
+large software book
+
+whiteSheetOfPaper()
+
+picture frame
+
+
+'''
 s = input().rstrip()
 op,clas,word = s.split(';')
 if op == 'S':
